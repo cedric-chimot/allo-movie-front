6999165399838aa367943702eb08f4172abf1826
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-10 17:15:50 +0200

    trouver un rôle par l'id

--- src/app/services/role/role.service.ts
+++ src/app/services/role/role.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Role } from '../../models/tables/Role';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleService {
+  private apiUrl = 'http://localhost:8080/api/role';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Trouver un rôle par son ID
+  getUserById(id: number): Observable<Role> {
+    return this.http.get<Role>(`${this.apiUrl}/${id}`);
+  }
+
 }
