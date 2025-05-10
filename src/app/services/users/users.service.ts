ec7fb79a67ed7e69f626e75902204ca8f9d90355
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-10 17:13:37 +0200

    paramétrage service users

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,9 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Users } from '../../models/tables/Users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+private apiUrl = 'http://localhost:8080/api/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Ajouter un nouvel utilisateur
+  createUser(user: Users): Observable<Users> {
+    return this.http.post<Users>(`${this.apiUrl}/create`, user);
+  }
+
+  // Afficher la liste de tous les utilisateurs
+  getAllUsers(): Observable<Users[]> {
+    return this.http.get<Users[]>(`${this.apiUrl}/all`);
+  }
+
+  // Trouver un utilisateur par son ID
+  getUserById(id: number): Observable<Users> {
+    return this.http.get<Users>(`${this.apiUrl}/${id}`);
+  }
+
+  // Récupère le nombre total d'utilisateurs
+  getNbUsers(): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/count`);
+  }
+
+  // Mettre à jour un utilisateur
+  updateUser(user: Users): Observable<Users> {
+    return this.http.put<Users>(`${this.apiUrl}/update`, user);
+  }
+
+  // Supprimer un utilisateur par son ID
+  deleteUserById(id: number): Observable<Users> {
+    return this.http.delete<Users>(`${this.apiUrl}/delete/${id}`);
+  }
+
+  // Supprimer tous les utilisateurs
+  deleteAllUsers(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/all`);
+  }
+
 }
