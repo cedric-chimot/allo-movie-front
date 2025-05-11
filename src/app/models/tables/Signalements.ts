7a9eae1752995518033a8babbf20df0bfc8236d4
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-11 10:21:07 +0200

    ajout d'une énum pour les motifs de signalements

--- src/app/models/tables/Signalements.ts
+++ src/app/models/tables/Signalements.ts
@@ -1,10 +1,11 @@
+import { MotifsEnum } from "./MotifsEnum";
 import { Reponses } from "./Reponses";
 import { Users } from "./Users";
 
 export class Signalements {
   constructor(
     public id: number,
-    public motif: string,
+    public motif: MotifsEnum,
     public dateSignalement: number,
     public message: string,
     public idUser: Users,
