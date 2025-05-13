a44afb9819f2110f67f3ee999605cb1b5b5d9bd4
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-13 10:27:33 +0200

    ajout des méthodes du service films

--- src/app/services/films/films.service.ts
+++ src/app/services/films/films.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Films } from '../../models/tables/Films';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmsService {
+  private apiUrl = 'http://localhost:8080/api/films';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Ajouter un nouveau film
+  createFilms(film: Films): Observable<Films> {
+    return this.http.post<Films>(`${this.apiUrl}/create`, film);
+  }
+
+  // Afficher la liste de tous les films
+  getAllFilms(): Observable<Films[]> {
+    return this.http.get<Films[]>(`${this.apiUrl}/all`);
+  }
+
+  // Trouver un film par son ID
+  getFilmsById(id: number): Observable<Films> {
+    return this.http.get<Films>(`${this.apiUrl}/${id}`);
+  }
+
+  // Mettre à jour un film
+  updateFilms(film: Films): Observable<Films> {
+    return this.http.put<Films>(`${this.apiUrl}/update`, film);
+  }
+
+  // Supprimer un film par son ID
+  deleteFilmsById(id: number): Observable<Films> {
+    return this.http.delete<Films>(`${this.apiUrl}/delete/${id}`);
+  }
+
+  // Supprimer tous les films
+  deleteAllFilms(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/all`);
+  }
+
 }
