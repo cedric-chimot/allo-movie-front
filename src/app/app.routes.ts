ac08f20b6e187b13df567bce62a238bcd4a8fd09
Cedric Chimot <cedric_om10@yahoo.fr>
2025-06-01 09:40:19 +0200

    ajout du composant homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginFormComponent } from './forms/login-form/login-form.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginFormComponent },
 ];
