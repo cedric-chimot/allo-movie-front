7d137b3d1a5f48585898297e251e7b913dca303e
Cedric Chimot <cedric_om10@yahoo.fr>
2025-06-02 09:38:29 +0200

    ajout du formulaire d'inscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginFormComponent } from './forms/login-form/login-form.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RegisterFormComponent } from './forms/register-form/register-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginFormComponent },
+  { path: 'register', component: RegisterFormComponent },
 ];
