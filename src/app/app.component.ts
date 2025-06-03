e2daf93fbfac4c5625800a1c18ab0627a8125dc0
Cedric Chimot <cedric_om10@yahoo.fr>
2025-06-03 09:51:36 +0200

    ajout des conditions pour l'affichage des navbars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
+import { NavbarComponent } from "./components/commons/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, RouterModule],
+  imports: [RouterOutlet, CommonModule, RouterModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
