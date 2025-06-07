0bfb30bd8c69c0685fe47364e4ecfac771ea2573
Cedric Chimot <cedric_om10@yahoo.fr>
2025-06-07 15:57:29 +0200

    ajustements sidebar et navbar admin, ajout page dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginFormComponent } from './forms/login-form/login-form.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { RegisterFormComponent } from './forms/register-form/register-form.component';
+import { AdminHomeComponent } from './pages/admin-pages/admin-home/admin-home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginFormComponent },
   { path: 'register', component: RegisterFormComponent },
+  { path: 'admin-home', component: AdminHomeComponent },
 ];
