4feea7e10f7ecc8e5ec2ce56d8bd00399097057f
Cedric Chimot <cedric_om10@yahoo.fr>
2025-06-07 15:47:01 +0200

    paramétrage de la navbar admin

--- src/app/components/admin/admin-navbar/admin-navbar.component.ts
+++ src/app/components/admin/admin-navbar/admin-navbar.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-admin-navbar',
-  imports: [],
+  imports: [CommonModule, RouterModule, NgbCollapseModule],
   templateUrl: './admin-navbar.component.html',
-  styleUrl: './admin-navbar.component.css'
+  styleUrls: ['./admin-navbar.component.css']
 })
-export class AdminNavbarComponent {
+export class AdminNavbarComponent implements OnInit {
 
+  toggleNavbar = true;
+  pseudo: string = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      this.toggleNavbar = true;
+    });
+
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      try {
+        const user = JSON.parse(userString);
+        this.pseudo = user.pseudo;
+      } catch (e) {
+        this.pseudo = '';
+      }
+    }
+  }
+
+  toggleNav() {
+    this.toggleNavbar = !this.toggleNavbar;
+  }
 }
