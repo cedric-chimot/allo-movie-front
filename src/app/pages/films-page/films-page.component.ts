7a0128672725b99c216408af0d2105fbe777b700
Cedric Chimot <cedric_om10@yahoo.fr>
2026-08-17 14:53:51 +0200

    création page films

---
+++ src/app/pages/films-page/films-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-films-page',
+  imports: [],
+  templateUrl: './films-page.component.html',
+  styleUrl: './films-page.component.css'
+})
+export class FilmsPageComponent {
+
+}
