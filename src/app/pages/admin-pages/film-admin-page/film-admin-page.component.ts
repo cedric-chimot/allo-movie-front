538d07701d2cfcbffbefbd0f685ab519fec38934
Cedric Chimot <cedric_om10@yahoo.fr>
2026-08-20 13:42:55 +0200

    préparation formulaire ajout de films

--- src/app/pages/admin-pages/film-admin-page/film-admin-page.component.ts
+++ src/app/pages/admin-pages/film-admin-page/film-admin-page.component.ts
@@ -4,10 +4,15 @@ import { RouterModule } from '@angular/router';
 
 import { Films } from '../../../models/tables/Films';
 import { FilmsService } from '../../../services/films/films.service';
+import { FilmFormComponent } from '../../../forms/film-form/film-form.component';
 
 @Component({
   selector: 'app-films-admin-page',
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FilmFormComponent
+  ],
   templateUrl: './film-admin-page.component.html',
   styleUrls: ['./film-admin-page.component.css']
 })
@@ -15,29 +20,40 @@ export class FilmsAdminPageComponent implements OnInit {
 
   films: Films[] = [];
 
-  constructor(private filmsService: FilmsService) {}
+  isAddModalOpen = false;
+
+  constructor(
+    private filmsService: FilmsService
+  ) {}
 
   ngOnInit(): void {
     this.chargerFilms();
   }
 
   chargerFilms(): void {
-
     this.filmsService.getAllFilms().subscribe({
-
       next: (films) => {
         this.films = films;
       },
-
       error: (erreur) => {
         console.error(
           'Erreur lors du chargement des films :',
           erreur
         );
       }
-
     });
-
   }
 
+  openAddModal(): void {
+    this.isAddModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isAddModalOpen = false;
+  }
+
+  filmAjoute(): void {
+    this.closeModal();
+    this.chargerFilms();
+  }
 }
