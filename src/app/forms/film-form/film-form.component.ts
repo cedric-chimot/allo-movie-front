ecebbc100246e602152da86c4f68af13d5671399
Cedric Chimot <cedric_om10@yahoo.fr>
2026-08-20 14:25:38 +0200

    stylisation du formulaire ajout film

--- src/app/forms/film-form/film-form.component.ts
+++ src/app/forms/film-form/film-form.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { Films } from '../../models/tables/Films';
+import { FilmsService } from '../../services/films/films.service';
 
 @Component({
   selector: 'app-film-form',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './film-form.component.html',
-  styleUrl: './film-form.component.css'
+  styleUrls: ['./film-form.component.css']
 })
 export class FilmFormComponent {
 
+  @Output() close = new EventEmitter<void>();
+  @Output() filmCreated = new EventEmitter<void>();
+
+  film: Films = new Films(
+    0,
+    '',
+    new Date(),
+    '',
+    '',
+    0
+  );
+
+  constructor(
+    private filmsService: FilmsService
+  ) {}
+
+  ajouterFilm(): void {
+
+    this.filmsService.createFilms(this.film).subscribe({
+      next: () => {
+        this.filmCreated.emit();
+      },
+      error: (erreur) => {
+        console.error(
+          'Erreur lors de la création du film :',
+          erreur
+        );
+      }
+    });
+
+  }
+
+  fermer(): void {
+    this.close.emit();
+  }
 }
