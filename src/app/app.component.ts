e19a85862c2a3e64315f65d3e7ea76660c1b0d7f
Cedric Chimot <cedric_om10@yahoo.fr>
2026-08-24 13:33:37 +0200

    modif tableau admin et gestion footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
 import { NavbarComponent } from "./components/commons/navbar/navbar.component";
-import { FooterComponent } from "./components/commons/footer/footer.component";
 import { AdminNavbarComponent } from "./components/admin/admin-navbar/admin-navbar.component";
 import { AdminSidebarComponent } from "./components/admin/admin-sidebar/admin-sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterModule, RouterOutlet, NavbarComponent, FooterComponent, AdminNavbarComponent, AdminSidebarComponent],
+  imports: [CommonModule, RouterModule, RouterOutlet, NavbarComponent, AdminNavbarComponent, AdminSidebarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
