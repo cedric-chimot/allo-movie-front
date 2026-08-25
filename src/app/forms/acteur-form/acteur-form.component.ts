8cd70001cc71738fd078f0942879bd6191877a02
Cedric Chimot <cedric_om10@yahoo.fr>
2026-08-25 13:26:39 +0200

    création des formulaires acteurs et catégories

---
+++ src/app/forms/acteur-form/acteur-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acteur-form',
+  imports: [],
+  templateUrl: './acteur-form.component.html',
+  styleUrl: './acteur-form.component.css'
+})
+export class ActeurFormComponent {
+
+}
