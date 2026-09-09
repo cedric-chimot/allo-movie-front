3ba97f4a24de770b43ed1ced1f874d0c04ecff92
Cedric Chimot <cedric_om10@yahoo.fr>
2026-09-09 13:08:14 +0200

    Formulaire de modification sur admin films

--- src/app/pages/admin-pages/film-admin-page/film-admin-page.component.ts
+++ src/app/pages/admin-pages/film-admin-page/film-admin-page.component.ts
@@ -5,14 +5,11 @@ import { RouterModule } from '@angular/router';
 import { Films } from '../../../models/tables/Films';
 import { FilmsService } from '../../../services/films/films.service';
 import { FilmFormComponent } from '../../../forms/film-form/film-form.component';
+import { FilmEditFormComponent } from '../../../forms/film-edit-form/film-edit-form.component';
 
 @Component({
   selector: 'app-films-admin-page',
-  imports: [
-    CommonModule,
-    RouterModule,
-    FilmFormComponent
-  ],
+  imports: [ CommonModule, RouterModule, FilmFormComponent, FilmEditFormComponent ],
   templateUrl: './film-admin-page.component.html',
   styleUrls: ['./film-admin-page.component.css']
 })
@@ -20,6 +17,8 @@ export class FilmsAdminPageComponent implements OnInit {
 
   films: Films[] = [];
   isAddModalOpen = false;
+  isEditModalOpen = false;
+  filmIdSelectionne!: number;
 
   // Pagination
   currentPage = 1;
@@ -68,8 +67,17 @@ export class FilmsAdminPageComponent implements OnInit {
     this.isAddModalOpen = true;
   }
 
+  openEditModal(filmId: number): void {
+    this.filmIdSelectionne = filmId;
+    this.isEditModalOpen = true;
+  }
+
   closeModal(): void {
     this.isAddModalOpen = false;
+  }
+
+  closeEditModal(): void {
+    this.isEditModalOpen = false;
   }
 
   filmAjoute(): void {
@@ -77,4 +85,11 @@ export class FilmsAdminPageComponent implements OnInit {
     this.currentPage = 1;
     this.chargerFilms();
   }
+
+  filmModifie(): void {
+    this.closeEditModal();
+    this.currentPage = 1;
+    this.chargerFilms();
+  }
+
 }
