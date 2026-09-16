54dae293c78f89380d727505bfd65919fba74467
Cedric Chimot <cedric_om10@yahoo.fr>
2026-09-16 13:39:47 +0200

    ajout pages admin users

---
+++ src/app/pages/admin-pages/users-admin-page/users-admin-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-admin-page',
+  imports: [],
+  templateUrl: './users-admin-page.component.html',
+  styleUrl: './users-admin-page.component.css'
+})
+export class UsersAdminPageComponent {
+
+}
